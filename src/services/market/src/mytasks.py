89a5bbdd30acbd4c150e7246b5291545796de74d
Mugna46 <a.mugnai4@studenti.unipi.it>
2024-11-26 18:20:20 +0100

    feat(market): update service calls to use HTTPS

--- src/services/market/src/mytasks.py
+++ src/services/market/src/mytasks.py
@@ -7,13 +7,13 @@ def add(x, y):
 
 @app.task
 def req():
-    r = requests.get(url="http://gacha_service:5000/collection")
+    r = requests.get(url="https://gacha_service:5000/collection")
     return r.text , r.status_code
 
 @app.task
 def invoke_payment(auction_uuid):
     try:
-        r = requests.post(url=f"http://market_service:5000/market/{auction_uuid}/payment")
+        r = requests.post(url=f"https://market_service:5000/market/{auction_uuid}/payment")
     except Exception as e:
         return str(e)
 
