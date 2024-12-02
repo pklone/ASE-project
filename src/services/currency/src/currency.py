00e4f9b6c85c01b21089ab42527daa278795f3bc
Mugna46 <a.mugnai4@studenti.unipi.it>
2024-12-02 18:41:18 +0100

    feat(currency): used bearer token for authentication instead of cookie, in a decorator function

--- src/services/currency/src/currency.py
+++ src/services/currency/src/currency.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify, abort, render_template
+from functools import wraps
 import requests
 import jwt
 import pybreaker
@@ -17,6 +18,38 @@ KEY_PATH = os.getenv("KEY_PATH")
 # set jwt
 SECRET = os.getenv("JWT_SECRET")
 
+def login_required(f):
+  @wraps(f)
+  def decorated_function(*args, **kwargs):
+      encoded_jwt = request.headers.get('Authorization')
+  
+      if not encoded_jwt:
+          return jsonify({'response': 'You\'re not logged'}), 401
+      
+      encoded_jwt = encoded_jwt.split(' ')[1]
+  
+      try:
+          options = {
+              'require': ['exp'], 
+              'verify_signature': True, 
+              'verify_exp': True
+          }
+  
+          decoded_jwt = jwt.decode(encoded_jwt, SECRET, algorithms=['HS256'], options=options)
+      except jwt.ExpiredSignatureError:
+          return jsonify({'response': 'Expired token'}), 403
+      except jwt.InvalidTokenError:
+          return jsonify({'response': 'Invalid token'}), 403
+  
+      if 'sub' not in decoded_jwt:
+          return jsonify({'response': 'Try later'}), 403
+  
+      additional = {'auth_uuid': decoded_jwt['sub']}
+  
+      return f(*args, **kwargs, **additional)
+  
+  return decorated_function
+
 @app.errorhandler(404)
 def page_not_found(error):
     return jsonify({'response': "page not found"}), 404
@@ -25,39 +58,18 @@ def page_not_found(error):
 def index():
     return render_template('buy.html'), 200
 
-@app.route('/currency/buy', methods=['POST'])
-def buy():
-    encoded_jwt = request.cookies.get('session')
+@app.route('/currency/buy', methods=['PUT'])
+@login_required
+def buy(auth_uuid):
+    amount = request.json.get('purchase')
 
-    if not encoded_jwt:
-        return jsonify({'response': 'You\'re not logged'}), 401
-
-    try:
-        options = {
-            'require': ['exp'], 
-            'verify_signature': True, 
-            'verify_exp': True
-        }
-
-        decoded_jwt = jwt.decode(encoded_jwt, SECRET, algorithms=['HS256'], options=options)
-    except jwt.ExpiredSignatureError:
-        return jsonify({'response': 'Expired token'}), 403
-    except jwt.InvalidTokenError:
-        return jsonify({'response': 'Invalid token'}), 403
-
-    if 'uuid' not in decoded_jwt:
-        return jsonify({'response': 'Try later'}), 403
-
-    player_uuid = decoded_jwt['uuid']
-    purchase = request.json.get('purchase')
-
-    player_purchasing = {
-        'player_uuid' : player_uuid,
-        'purchase' : purchase
-    }
+    if amount <= 0:
+        return jsonify({'response': 'Invalid purchase'}), 400
+    
+    amount = {'amount': amount}
 
     try:
-        r = circuitbreaker.call(requests.post, 'https://player_service:5000/currency/buy', json=player_purchasing, verify=False)
+        r = circuitbreaker.call(requests.put, f'https://player_service:5000/{auth_uuid}/wallet', json=amount, verify=False)
     except Exception as e:
         return jsonify({'response': str(e)}), 500
     
