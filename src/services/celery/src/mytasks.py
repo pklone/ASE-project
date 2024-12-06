eb629f48436b56ea252145577673f874246406af
Mugna46 <a.mugnai4@studenti.unipi.it>
2024-12-06 04:13:20 +0100

    feat(celery): implement circuit breaker for request handling

--- src/services/celery/src/mytasks.py
+++ src/services/celery/src/mytasks.py
@@ -1,5 +1,11 @@
 from mycelery import app
 import requests
+import pybreaker
+
+circuitbreaker = pybreaker.CircuitBreaker(
+            fail_max=5, 
+            reset_timeout=60*5
+        )
 
 @app.task
 def add(x, y):
@@ -7,13 +13,13 @@ def add(x, y):
 
 @app.task
 def req():
-    r = requests.get(url="https://gacha_service:5000/collection", verify=False) # nosec B501 We are using self signed certificates
+    r = circuitbreaker.call(requests.get, "https://gacha_service:5000/collection", verify=False) # nosec B501 We are using self signed certificates
     return r.text, r.status_code
 
 @app.task
 def invoke_payment(auction_uuid):
     try:
-        r = requests.post(url=f"https://market_service:5000/market/{auction_uuid}/payment", verify=False) # nosec B501 We are using self signed certificates
+        r = circuitbreaker.call(requests.post, f"https://market_service:5000/market/{auction_uuid}/payment", verify=False) # nosec B501 We are using self signed certificates
     except Exception as e:
         return str(e), 500
 
